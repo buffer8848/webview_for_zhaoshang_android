b0329a6bcad86db65aa24dfd91af27869d6138c5
binfeilx <binfeix.li@intel.com>
2014-09-05 17:34:24 +0800

    initialize webview for zhaoshang.net v0.2

---
+++ src/com/example/tjzsweb/MainActivity.java
@@ -0,0 +1,165 @@
+package com.example.tjzsweb;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+
+import android.view.KeyEvent;  
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.WebView;  
+import android.webkit.WebViewClient;  
+import android.widget.Toast;
+import android.widget.ImageView;
+
+
+public class MainActivity extends Activity {
+	private String url = "http://www.zhaoshang.net/m/redirect.php";
+	private String cookiefile = "cookie.data";
+	
+	private WebView mWebView; 
+	private ImageView mImageView;
+	private ProgressDialog progressBar; 
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE); //show on page not the title
+		setContentView(R.layout.activity_main);
+		
+		//loading image animate
+		mImageView = (ImageView) findViewById(R.id.imageView1);
+		new Handler().postDelayed(new Runnable() 
+		{  
+            public void run() 
+            {  
+            	mImageView.setImageBitmap(null);
+                mImageView.destroyDrawingCache(); 
+                mWebView.invalidate();
+            }  
+  
+        }, 3000); 
+		
+		//load cookie
+		String cookies = null;
+		FileInputStream inStream = null;
+		try {
+			inStream = getBaseContext().openFileInput(cookiefile);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		if (inStream != null)
+		{
+			byte[] bytes = new byte[1024];  
+	        ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();  
+	        try {
+				while (inStream.read(bytes) != -1) {  
+				    arrayOutputStream.write(bytes, 0, bytes.length);  
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}  
+	        try {
+				inStream.close();
+				arrayOutputStream.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	        cookies = new String(arrayOutputStream.toByteArray()); 
+		}
+        
+		//set cookie
+		CookieSyncManager.createInstance(this.getBaseContext());
+		CookieManager cookieManager = CookieManager.getInstance();
+		cookieManager.setAcceptCookie(true);
+		cookieManager.setCookie(url, cookies);
+		CookieSyncManager.getInstance().sync();
+		
+		//load url
+		mWebView = (WebView) findViewById(R.id.webView1);  
+		mWebView.getSettings().setJavaScriptEnabled(true); 
+		mWebView.getSettings().setBuiltInZoomControls(true);
+		mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY); 
+		
+		progressBar = ProgressDialog.show(this, null, "Loading..."); 
+		
+		mWebView.setWebViewClient(new WebViewClient(){
+			@Override
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				progressBar.show();
+			}
+			
+			@Override
+            public void onPageFinished(WebView view, String url)
+            {
+				CookieSyncManager cookieSyncManager = CookieSyncManager.createInstance(MainActivity.this);
+				cookieSyncManager.sync();
+				CookieManager cookieManager = CookieManager.getInstance();
+				cookieManager.setAcceptCookie(true);
+				FileOutputStream outStream = null;
+				try {
+					getBaseContext();
+					outStream = openFileOutput(cookiefile, Context.MODE_PRIVATE);
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return;
+				}
+				try {
+					if (cookieManager.getCookie(url) != null)
+						outStream.write(cookieManager.getCookie(url).getBytes());
+					outStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				progressBar.hide();
+            }
+            
+            @Override
+            public void onReceivedError(WebView view, int errorCode,  
+                    String description, String failingUrl) {  
+                // TODO Auto-generated method stub  
+                Toast.makeText(MainActivity.this, "Oh no! " + description, Toast.LENGTH_SHORT).show(); 
+            }  
+        });
+		mWebView.loadUrl(url);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override 
+    public boolean onKeyDown(int keyCode, KeyEvent event) {  
+        // TODO Auto-generated method stub  
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {  
+        	mWebView.goBack();  
+            return true;  
+        }  
+        return super.onKeyDown(keyCode, event);  
+    }  
+
+}
