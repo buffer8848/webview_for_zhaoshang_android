2fa29f0e72aafd33580e4212765fe98c84e6191b
buffer8848 <179770346@qq.com>
2015-04-30 13:17:17 +0800

    support tel

--- src/com/example/tjzsweb/MainActivity.java
+++ src/com/example/tjzsweb/MainActivity.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.app.Activity;
@@ -114,6 +115,18 @@ public class MainActivity extends Activity {
 		mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY); 
 		mWebView.setWebViewClient(new WebViewClient()
 		{
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view,String url){    
+				//当有新连接时，使用当前的 WebView    
+				view.loadUrl(url);  
+				//调用拨号程序  
+				if (url.startsWith("mailto:") || url.startsWith("geo:") ||url.startsWith("tel:")) {  
+				  Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));  
+				   startActivity(intent);  
+				  }  
+				return true;    
+			}
+			
 			@Override
 			public void onPageStarted(WebView view, String url, Bitmap favicon) 
 			{
